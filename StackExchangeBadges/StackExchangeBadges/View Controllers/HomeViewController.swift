dbb7fc319bfdf6acbc65a7bce0b4fe90cad3bcc7
Pablo J. Bertola <pbertola@belatrixsf.com>
2020-05-31 18:20:58 -0300

    Base project

---
+++ StackExchangeBadges/StackExchangeBadges/View Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackExchangeBadges
+//
+//  Created by Pablo Javier Bertola on 31/05/2020.
+//  Copyright © 2020 Pablo Javier Bertola. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
